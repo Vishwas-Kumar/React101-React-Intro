dd04f87900d253917f08a132d648c27213a7b44f
Vishwas-Kumar <vishkkb@gmail.com>
2022-05-16 21:46:44 +0530

    Navbar

---
+++ src/component/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+let Navbar = () => {
+    return (
+    <div className="navbar">
+        <div className="logo">
+            <h2>LOGOBAKERY</h2>
+        </div>
+        <div className="spa">
+            <p>Services</p>
+            <p>Projects</p>
+            <p>About</p>
+        </div>
+        <div>
+            <button>Contact</button>
+        </div>
+    </div>
+    )
+}
+export default Navbar
